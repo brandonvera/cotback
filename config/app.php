ccd93c6e9182621cfce3ec38ac9e1a14d61f9604
brandonvera <vera.950b@gmail.com>
2022-08-16 17:01:49 -0400

    Paquete Inliner CSS para estilos en vistas decorreos

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Fedeisas\LaravelMailCssInliner\LaravelMailCssInlinerServiceProvider::class,
 
         /*
          * Application Service Providers...
